5ca94914ce580c179275eda5cd8fb758f5a17b47
Sunday Football Day <abrhamkassahun250@gmail.com>
2026-07-06 17:04:17 +0300

    Add registration details for multiple users

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ First Name
 Abrham.D
 
 [Check Result]
+
+Registration Number
+
+30
+
+First Name
+
+Abrham(Vandak)
+
+[Check Result]
+
+Registration Number
+
+24
+
+First Name
+
+Amen
+
+[Check Result]
+
+Registration Number
+
+08
+
+First Name
+
+Abrham.K
+
+[Check Result]
