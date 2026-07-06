474e8eaa64c12e9cc9222c7cdf9d93bc4768166b
Sunday Football Day <abrhamkassahun250@gmail.com>
2026-07-06 17:10:47 +0300

    Modify registration numbers and first names
    Updated registration numbers and first names in the script.

--- script.js
+++ script.js
@@ -40,10 +40,74 @@ Amen
 
 Registration Number
 
-08
+09
 
 First Name
 
-Abrham.K
+Absalew
+
+[Check Result]
+
+03
+
+First Name
+
+Yabsira
+
+[Check Result]
+
+07
+
+First Name
+
+Yidnekachew
+
+[Check Result]
+
+11
+
+First Name
+
+Firaol
+
+[Check Result]
+
+19
+
+First Name
+
+Leykun
+
+[Check Result]
+
+37
+
+First Name
+
+Mikiyas.S
+
+[Check Result]
+
+18
+
+First Name
+
+Yabsira
+
+[Check Result]
+
+30
+
+First Name
+
+Natanim
+
+[Check Result]
+
+47
+
+First Name
+
+Natan
 
 [Check Result]
